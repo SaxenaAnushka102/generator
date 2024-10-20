eed35268e35f7c0c0fed0cfdbf03b75536ea8af7
ANUSHKA SAXENA <anushka.saxena-it-2k19-05@iips.edu.in>
2024-10-20 18:48:50 +0000

    Write script to run the test code

--- python-mqtt-client-template/__transpiled/index.js
+++ python-mqtt-client-template/__transpiled/index.js
@@ -5,12 +5,24 @@ var generatorReactSdk = require('@asyncapi/generator-react-sdk');
 var jsxRuntime = require('/usr/local/share/nvm/versions/node/v20.17.0/lib/node_modules/@asyncapi/cli/node_modules/react/cjs/react-jsx-runtime.production.min.js');
 
 function index ({
-  asyncapi
+  asyncapi,
+  params
 }) {
-  //3
   return /*#__PURE__*/jsxRuntime.jsx(generatorReactSdk.File, {
     name: "client.py",
-    children: asyncapi.info().title()
+    children: `import paho.mqtt.client as mqtt
+
+mqttBroker = "${asyncapi.servers().get(params.server).url()}"
+
+class TemperatureServiceClient:
+    def __init__(self):
+        self.client = mqtt.Client()
+        self.client.connect(mqttBroker)
+
+
+    def sendTemperatureChange(self, id):
+        topic = "temperature/changed"
+        self.client.publish(topic, id)`
   });
 }
 
--- python-mqtt-client-template/template/index.js
+++ python-mqtt-client-template/template/index.js
@@ -1,7 +1,23 @@
-//1
-import { File } from '@asyncapi/generator-react-sdk'
-//2
-export default function ({ asyncapi }) {
-//3
-  return <File name="client.py">{asyncapi.info().title()}</File>
+import { File } from '@asyncapi/generator-react-sdk';
+
+// notice that now the template not only gets the instance of parsed AsyncAPI document but also the parameters
+export default function ({ asyncapi, params }) {
+ 
+  return (
+    <File name="client.py">
+      {`import paho.mqtt.client as mqtt
+
+mqttBroker = "${asyncapi.servers().get(params.server).url()}"
+
+class TemperatureServiceClient:
+    def __init__(self):
+        self.client = mqtt.Client()
+        self.client.connect(mqttBroker)
+
+
+    def sendTemperatureChange(self, id):
+        topic = "temperature/changed"
+        self.client.publish(topic, id)`}
+    </File>
+  )
 }
