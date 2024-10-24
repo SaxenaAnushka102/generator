1de7e2d6d62bf3eec89329bdc84aaba143c5bca4
ANUSHKA SAXENA <anushka.saxena-it-2k19-05@iips.edu.in>
2024-10-24 15:35:53 +0000

    Create a reusable component and update AsyncAPI document

--- python-mqtt-client-template/template/index.js
+++ python-mqtt-client-template/template/index.js
@@ -1,23 +1,24 @@
-import { File } from '@asyncapi/generator-react-sdk';
+import { File, Text } from '@asyncapi/generator-react-sdk'
+import { TopicFunction } from '../components/TopicFunction'
 
-// notice that now the template not only gets the instance of parsed AsyncAPI document but also the parameters
 export default function ({ asyncapi, params }) {
- 
   return (
     <File name="client.py">
-      {`import paho.mqtt.client as mqtt
+      <Text newLines={2}>import paho.mqtt.client as mqtt</Text>
 
-mqttBroker = "${asyncapi.servers().get(params.server).url()}"
+      <Text newLines={2}>mqttBroker = "{asyncapi.servers().get(params.server).url()}"</Text>
 
-class TemperatureServiceClient:
-    def __init__(self):
-        self.client = mqtt.Client()
-        self.client.connect(mqttBroker)
+      <Text newLines={2}>class {asyncapi.info().title().replaceAll(' ', '')}Client:</Text>
 
+      <Text indent={2} newLines={2}>
+        {`def __init__(self):
+            self.client = mqtt.Client()
+            self.client.connect(mqttBroker)`}
+      </Text>
 
-    def sendTemperatureChange(self, id):
-        topic = "temperature/changed"
-        self.client.publish(topic, id)`}
+      <Text indent={2}>
+        <TopicFunction channels={asyncapi.channels().filterByReceive()} />
+      </Text>
     </File>
   )
 }
